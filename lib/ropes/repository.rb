a43fcf74ffd47ede5d98ab2c1d7e97d382b803ac
Andy Sykes <github@tinycat.co.uk>
2013-10-19 14:48:03 +0100

    Rework how files are generated

--- lib/ropes/repository.rb
+++ lib/ropes/repository.rb
@@ -1,4 +1,8 @@
 require 'debeasy'
+require 'stringio'
+require 'zlib'
+require 'tempfile'
+require 'digest'
 
 module Ropes
 
@@ -15,14 +19,18 @@ module Ropes
         version 
         architecture 
         components 
-        description}.reject do |required_option|
-          options.has_key?(required_option)
+        description
+        package_base}.reject do |required_option|
+          options.has_key?(required_option.to_sym)
       end
 
-      raise "Missing options: #{missing_options.join(", ")}"
+      raise "Missing options: #{missing_options.join(", ")}" unless missing_options.empty?
 
-      raise InvalidRepositoryType, 
-        "Repository type #{options[:type]} is neither :apt or :yum" unless options[:type] == :yum or type == :apt
+      raise InvalidRepositoryType, "Repository type #{options[:type]} is neither :apt or :yum" unless options[:type] == :yum or options[:type] == :apt
+
+      @release_file     = nil
+      @packages_file     = nil
+      @packages_field_gz = nil
 
       @options = options
       @packages = []
@@ -41,11 +49,11 @@ module Ropes
         MD5sum
         SHA1
         SHA256
-        Description
-        Description-md5
-        Bugs
-        Origin
-        Supported
+        description
+        description-md5
+        bugs
+        origin
+        supported
       }
       @mandatory_fields = %w{
         package
@@ -77,23 +85,75 @@ module Ropes
       end
     end
 
+    # Get the Packages file as a string
+
+    def packages_file
+      return @packages_file unless @packages_file.nil?
+      entries = @packages.map do |package|
+        lines = []
+        @field_order.each do |field|
+          if package[field] != nil
+            case field
+            when "filename"
+              lines << packages_line(field.capitalize, "#{@options[:package_base]}/#{package[field]}") 
+            when "installed_size"
+              lines << packages_line("Installed-Size", package[field])
+            when "SHA1", "SHA256", "MD5sum"
+              lines << packages_line(field, package[field])
+            else
+              lines << packages_line(field.capitalize, package[field])
+            end
+          end
+        end
+        lines.join("\n")
+      end
+      @packages_file = entries.join("\n\n")
+    end
+
+    # Get the Packages file as a gzip'ed string
+
+    def packages_file_gz
+      io = StringIO.new("w")
+      gz = Zlib::GzipWriter.new(io)
+      gz.write(packages_file())
+      gz.close
+      io.string
+    end
+
+    # Get the Release file as a string
+
+    def release_file 
+
+      # Have to create the files in real life to get the real size
+      temp_packages_file = Tempfile.new("Packages")
+      temp_packages_file.write packages_file
+      temp_packages_file_gz= Tempfile.new("Packages.gz")
+      temp_packages_file_gz.write packages_file_gz
+
+      lines = []
+      lines << "Origin: #{@options[:origin]}"
+      lines << "Label: #{@options[:origin]}"
+      lines << "Suite: #{@options[:distribution]}"
+      lines << "Version: #{@options[:version]}"
+      lines << "Codename: #{@options[:distribution]}"
+      lines << "Date: #{Time.new.utc.strftime '%a, %d %b %Y %H:%M:%S UTC'}"
+      lines << "Architectures: #{@options[:architecture]}"
+      lines << "Components: #{@options[:components]}"
+      lines << "Description: #{@options[:description]}"
+      lines << "MD5Sum:"
+      lines << " #{Digest::MD5.hexdigest(packages_file)}                   #{temp_packages_file.size} #{@options[:components]}/binary-#{@options[:architecture]}/Packages"
+      lines << " #{Digest::MD5.hexdigest(packages_file_gz)}                   #{temp_packages_file_gz.size} #{@options[:components]}/binary-#{@options[:architecture]}/Packages.gz"
+      lines.join("\n")
+    end
+
+    private
+
+    def packages_line(field, value)
+      "#{field}: #{value}"
+    end
+
     def validate_metadata(metadata_hash)
       @mandatory_fields.reject {|field| metadata_hash.has_key?(field)}
-    end
-
-    def write_files(path)
-      write_packages_gz("#{path}/Packages.gz")
-    end
-
-    def write_packages_gz(path)
-      Zlib::GzipWriter.open(path) do |file|
-        @packages.each do |package|
-          @field_order.each do |field_order|
-            file.puts "#{field_order.capitalize}: #{package[field_order]}"
-          end
-          file.puts
-        end
-      end
     end
 
   end
